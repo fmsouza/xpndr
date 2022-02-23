190b65a5e531a4487e94e62c46c69e5ab4b63ccf
Fred Souza <fredericoamsouza@gmail.com>
2022-02-23 19:22:51 -0300

    Example test

---
+++ src/home/screens/__tests__/HomeScreen.spec.tsx
@@ -0,0 +1,25 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { Button } from '~/shared/components';
+
+import {HomeScreen} from '../HomeScreen';
+
+jest.mock('@react-navigation/native', () => ({
+  ...jest.requireActual('@react-navigation/native'),
+  useTheme: jest.fn(() => ({
+    dimensions: {},
+    colors: {}
+  })),
+  useNavigation: jest.fn(() => ({
+    navigate: jest.fn(),
+    setOptions: jest.fn()
+  })),
+}));
+
+it('renders correctly', () => {
+  const testRenderer = renderer.create(<HomeScreen />);
+  const instance = testRenderer.root;
+  expect(instance.findByType(Button)).toBeDefined();
+});
