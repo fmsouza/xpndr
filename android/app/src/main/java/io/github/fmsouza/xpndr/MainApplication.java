f6b10e777ea14f6f9313ece0a6d6cdcbdf124bbc
Fred Souza <fredericoamsouza@gmail.com>
2022-02-24 00:02:11 -0300

    Renaming the app

--- android/app/src/main/java/io/github/fmsouza/xpndr/MainApplication.java
+++ android/app/src/main/java/io/github/fmsouza/xpndr/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rnboilerplate;
+package io.github.fmsouza.xpndr;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.rnboilerplate.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("io.github.fmsouza.xpndr.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- ios/XpndrTests/XpndrTests.m
+++ ios/XpndrTests/XpndrTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNBoilerplateTests : XCTestCase
+@interface XpndrTests : XCTestCase
 
 @end
 
-@implementation RNBoilerplateTests
+@implementation XpndrTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
