4635a64c44cabf355dfd3504255ceaabaec5b3ec
Fred Souza <fredericoamsouza@gmail.com>
2022-03-16 20:20:06 -0300

    Refactoring users resolvers

--- server/src/users/index.ts
+++ server/src/users/index.ts
@@ -1,1 +1,5 @@
-export * from './resolvers';
+import * as usersResolvers from './resolvers';
+
+export const resolvers = [
+  ...Object.values(usersResolvers)
+];
--- server/src/users/resolvers.ts
+++ server/src/users/resolvers.ts
@@ -2,21 +2,45 @@ import {
   Resolver,
   Mutation,
   Arg,
+  InputType,
+  Field,
+  Ctx,
+  Authorized,
+  Query,
 } from 'type-graphql'
 import { Service } from 'typedi'
 
-import { AuthResponse, User } from '../types'
-import { AuthService, UsersService } from '../services'
+import { AuthResponse, User } from './types'
+import { AuthService, UsersService } from './services'
+import { Context } from '../context';
 
-import { UserCreateInput, UserLoginInput } from './inputs'
+@InputType()
+export class UserCreateInput {
+  @Field() email: string;
+  @Field() name: string;
+  @Field() password: string;
+}
+
+@InputType()
+export class UserLoginInput {
+  @Field() email: string;
+  @Field() password: string;
+}
 
 @Service()
 @Resolver(User)
-export class MutationsResolver {
+export class UsersResolvers {
   public constructor(
     private readonly authService: AuthService,
     private readonly usersService: UsersService,
   ) { }
+
+  @Authorized()
+  @Query(() => User)
+  public async me(@Ctx() context: Context) {
+    const { user } = context;
+    return this.usersService.getUserById(user.id);
+  }
 
   @Mutation((returns) => User)
   public async signup(
