7ada39bcf1edccb2c61018bdb9b11d79ee2fd385
Fred Souza <fredericoamsouza@gmail.com>
2022-03-25 21:24:18 -0300

    Configuring postgres

--- server/prisma/migrations/20220326001337_/migration.sql
+++ server/prisma/migrations/20220326001337_/migration.sql
@@ -1,42 +1,46 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "accounts" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
-    "deletedAt" DATETIME,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
     "connectionDetails" TEXT,
     "accountType" TEXT NOT NULL,
     "ownerId" INTEGER NOT NULL,
-    CONSTRAINT "accounts_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "transactions_account" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "externalId" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "amount" INTEGER NOT NULL,
     "type" TEXT NOT NULL,
     "movement" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
     "accountId" INTEGER NOT NULL,
-    CONSTRAINT "transactions_account_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "accounts" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "transactions_account_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "transactions_credit_card" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "externalId" TEXT NOT NULL,
     "amount" INTEGER NOT NULL,
     "installments" INTEGER,
@@ -44,9 +48,10 @@ CREATE TABLE "transactions_credit_card" (
     "category" TEXT NOT NULL,
     "isForeign" BOOLEAN NOT NULL,
     "online" BOOLEAN NOT NULL,
-    "createdAt" DATETIME NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
     "accountId" INTEGER NOT NULL,
-    CONSTRAINT "transactions_credit_card_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "accounts" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "transactions_credit_card_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -69,3 +74,12 @@ CREATE INDEX "transactions_credit_card_externalId_idx" ON "transactions_credit_c
 
 -- CreateIndex
 CREATE UNIQUE INDEX "transactions_credit_card_externalId_key" ON "transactions_credit_card"("externalId");
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions_account" ADD CONSTRAINT "transactions_account_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions_credit_card" ADD CONSTRAINT "transactions_credit_card_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "accounts"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
