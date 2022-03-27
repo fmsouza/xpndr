717487537af2f69622e1e95bdf384cc94778cb31
Fred Souza <fredericoamsouza@gmail.com>
2022-03-27 16:23:32 -0300

    Throws a custom error when auth fails

--- server/src/auth-checker.ts
+++ server/src/auth-checker.ts
@@ -1,9 +1,15 @@
 import { ResolverData } from "type-graphql";
 
+import { AuthenticationRequiredError } from "~/shared/errors";
+
 import { Context } from "./context";
 
 export default async function({ context }: ResolverData<Context>, _roles: any[]): Promise<boolean> {
   const { user } = context ?? {};
+
+  if (!user) {
+    throw new AuthenticationRequiredError('Access Forbidden: You must authenticate first.');
+  }
 
   return Boolean(user);
 }
--- server/src/shared/errors.ts
+++ server/src/shared/errors.ts
@@ -1,5 +1,12 @@
 import { ApolloError } from 'apollo-server';
 
+export class AuthenticationRequiredError extends ApolloError {
+  public constructor(message: string) {
+    super(message, 'AUTHENTICATION_REQUIRED');
+    Object.defineProperty(this, 'name', { value: 'AuthenticationRequiredError' });
+  }
+}
+
 export class ResourceNotFoundError extends ApolloError {
   public constructor(message: string) {
     super(message, 'RESOURCE_NOT_FOUND');
