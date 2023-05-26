c31ca55f015db219eb3df300d691faae8136b32c
Fred Souza <fredericoamsouza@gmail.com>
2023-05-25 22:17:42 -0300

    Setting up default route

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,10 +1,12 @@
-import { useRoutes } from 'react-router-dom';
+import { Navigate, useRoutes } from 'react-router-dom';
 
 import {Routes as AuthRoutes} from './modules/auth/routes';
 import {Routes as AccountRoutes} from './modules/account/routes';
+import { ProtectedRoute } from './modules/auth/components';
 
 export const Routes = () => {
   return useRoutes([
+    { path: '/', Component: ProtectedRoute(() => <Navigate to="/account/dashboard" replace />)},
     AuthRoutes,
     AccountRoutes,
   ]);
